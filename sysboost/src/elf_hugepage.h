246f4b86f8883f3b81843ec391eb08b9fcea40f0
Tie Liu <liutie4@huawei.com>
2023-04-04 20:02:41 +0800

    sysboost: Add a tag for using hugepage
    elf_set_hugepage:
    	Mark executable segments in ELF files, including code segments and
    read-only data segments, as using huge pages.

---
+++ sysboost/src/elf_hugepage.h
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: MulanPSL-2.0 */
+#ifndef _ELF_HUGEPAGE_H
+#define _ELF_HUGEPAGE_H
+
+#include "elf_link_common.h"
+
+void elf_set_hugepage(elf_link_t *elf_link);
+
+#endif /* _ELF_HUGEPAGE_H */
