ca283aa77e297420f9b9aee28c59dd73860b4f43
Tie Liu <liutie4@huawei.com>
2023-04-04 19:57:27 +0800

    sysboost: provides a function to check whether the address assignment of the generated ELF file is correct
    elf_check_elf:
    	Used to check whether the addresses of the .bss, .data, and GNU_RELRO
    sections are correct.

---
+++ sysboost/src/elf_check_elf.h
@@ -0,0 +1,13 @@
+/* SPDX-License-Identifier: MulanPSL-2.0 */
+#ifndef _ELF_CHECK_ELF_H
+#define _ELF_CHECK_ELF_H
+
+#include <stdbool.h>
+#include <stdint.h>
+
+#include "elf_link_common.h"
+#include "si_common.h"
+
+void elf_check_elf(elf_link_t *elf_link);
+
+#endif /* _ELF_CHECK_ELF_H */
