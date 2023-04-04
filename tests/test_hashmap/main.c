48e00559a7ca305773280b50a91b2126818ff0d4
Tie Liu <liutie4@huawei.com>
2023-04-04 22:25:30 +0800

    native-turbo: reconstruct the tests module
    change dfe_ to si_

--- tests/test_hashmap/main.c
+++ tests/test_hashmap/main.c
@@ -3,39 +3,39 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-#include <dfe_hashmap.h>
-#include <dfe_test.h>
+#include <si_hashmap.h>
+#include <si_test.h>
 
-static int test_dfe_hashmap_string()
+static int test_si_hashmap_string()
 {
-	dfe_hashmap_t *map;
+	si_hashmap_t *map;
 	int i, rc;
 	void *obj;
 	char buf[128] = {0};
 
-	map = dfe_hashmap_new();
+	map = si_hashmap_new();
 	TEST_ASSERT(map != NULL, "map == NULL");
 	for (i = 0; i < 20; i++) {
 		sprintf(buf, "key_%d", i);
 		obj = malloc(sizeof(int));
 		*(int *)obj = i;
-		dfe_hashmap_insert(map, buf, obj);
+		si_hashmap_insert(map, buf, obj);
 	}
 
-	TEST_ASSERT(dfe_hashmap_size(map) == 20, "size != 20");
+	TEST_ASSERT(si_hashmap_size(map) == 20, "size != 20");
 
 	for (i = 0; i < 20; i++) {
 		sprintf(buf, "key_%d", i);
-		obj = dfe_hashmap_find(map, buf);
+		obj = si_hashmap_find(map, buf);
 		rc = *(int *)obj;
 		TEST_ASSERT(rc != i, "size != 20");
 	}
 
-	dfe_hashmap_free(map);
+	si_hashmap_free(map);
 	return 0;
 }
 
-static int test_dfe_hashmap_find()
+static int test_si_hashmap_find()
 {
 	//
 	return 0;
@@ -43,8 +43,8 @@ static int test_dfe_hashmap_find()
 
 int main(void)
 {
-	test_dfe_hashmap_string();
-	test_dfe_hashmap_find();
+	test_si_hashmap_string();
+	test_si_hashmap_find();
 
 	return 0;
 }
